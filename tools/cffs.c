eaf7ed8fdfccb003333ee3ad3f0f2d5044bc26dd
Simon Evans <si@si.org>
2002-07-04 00:07:56 +0000

    remove crap process flags

--- tools/cffs.c
+++ tools/cffs.c
@@ -13,79 +13,58 @@
 
 #include "../fs/fileheader.h"
 
-#if 0
-#define POLY (1<<12 | 1<< 5 | 1)
-uint16_t calc_crc16(uint8_t *buf, int len, uint16_t wcrc)
+/* 16bit check sum calculation */
+uint16_t calc_chk16(uint8_t *buf, int len)
 {
-	uint16_t crc = 0xffff;
-	uint8_t byte, bit;
-	int idx;
-	uint16_t poly;
-	uint8_t *p = buf;
-
-	for(poly = 0; poly != 0xffff; poly++) {
-		buf = p;
-		for (idx = 0; idx < len; idx++) {
-			for (byte = *buf++, bit = 0; bit < 8; bit++, byte >>= 1)
-				crc = (crc >> 1) ^ (((crc ^ byte) & 1) ? poly : 0);
-		}
-		if(crc == ~wcrc) {
-			printf("Found CRC with poly = 0x%4.4X\n", (uint16_t)poly);
-			//return crc;
-		}
-	}
-	return 0;
-}
-#else
-
-uint16_t calc_crc16(uint8_t *buf, int len, uint16_t wcrc)
-{
-	uint32_t crc = 0;
+	uint32_t chk = 0;
 	uint16_t d, *data;
 	
 	data = (uint16_t *)buf;
 
-	printf("len = %d\n", len);
 	while(len & ~1) {
 		d = ~ntohs(*(data++));
-		crc += (uint16_t)d;
-		crc = (crc & 0xffff) + (crc >> 16);
+		chk += (uint16_t)d;
+		chk = (chk & 0xffff) + (chk >> 16);
 		len -= 2;
 	}
-#if 1
+
 	if(len) {
-		crc += (uint16_t)~(*buf << 8);
-/*
-		d = (*buf << 8) & 0xffff;
-		printf("Handling odd byte crc = 0x%8.8X d=0x%4.4X, ~d=0x%4.4X\n", crc, d, ~d);
-		
-		//d &= 0xff;
-		crc += (uint16_t)~d;
-*/
-		crc = (crc & 0xffff) + (crc >> 16);
+		chk += (uint16_t)~(*data << 8);
+		chk = (chk & 0xffff) + (chk >> 16);
 	}
-#endif
-		
-	//printf("crc = 0x%8.8X, ~crc = 0x%8.8X\n", crc, ~crc);
-	return (uint16_t)crc;
+	return (uint16_t)chk;
 }
-#endif
+
 
 void swap_header(ciscoflash_filehdr *h)
 {
 	h->length = ntohl(h->length);
-	h->crc = ntohs(h->crc);
+	h->chksum = ntohs(h->chksum);
 	h->flags = ntohs(h->flags);
 	h->date = ntohl(h->date);
 }
 
 
-void dump_header(ciscoflash_filehdr *h)
+void dump_header(ciscoflash_filehdr *h, uint16_t chk)
 {
 	time_t t = (time_t)h->date;
-	printf("magic: 0x%8.8X length: %u crc: 0x%4.4X flags: 0x%4.4X\n",
-	       h->magic, h->length, (uint16_t)h->crc, (uint16_t)h->flags);
-	printf("date: %s name: %s\n", ctime(&t), h->name);
+	struct tm tm;
+	char timebuf[16];
+
+	localtime_r(&t, &tm);
+
+	if(!(h->flags & FLAG_HASDATE))
+		strftime(timebuf, 15, "%b %d %H:%M", &tm);
+	else
+		strcpy(timebuf, "  <no date> ");
+
+	   
+	printf("%10d %s [%4.4X] [%4.4X] %s %s %s\n", h->length, timebuf, h->chksum, h->flags, h->name,
+	       !(h->flags & FLAG_DELETED) ? "[deleted]" : "",
+	       (chk != h->chksum) ? "[bad chksum]" : "");
+	//printf("magic: 0x%8.8X length: %u chk: 0x%4.4X flags: 0x%4.4X\n",
+	//       h->magic, h->length, (uint16_t)h->chksum, (uint16_t)h->flags);
+	//printf("date: %s name: %s\n", ctime(&t), h->name);
 }
 
 
@@ -107,7 +86,8 @@ int main(int argc, char **argv)
 	uint32_t *magic;
 	char buf[sizeof(ciscoflash_filehdr_ext)];
 	char *p;
-
+	int eof = 0;
+	
 	if(argc > 2 && !strcmp(*(argv+1), "-d"))
 	   device = *(argv+2);
 
@@ -125,40 +105,43 @@ int main(int argc, char **argv)
 	}
 	lseek(fd, 0, 0);
 
-	while(read(fd, &buf, sizeof(magic)) == sizeof(magic)) {
+	while(!eof && read(fd, &buf, sizeof(magic)) == sizeof(magic)) {
 		int len;
 		magic = (uint32_t *)buf;
 		*magic = ntohl(*magic);
-		if(*magic == CISCO_FH_MAGIC || ntohl(*magic) == CISCO_FH_MAGIC)
+
+		switch(*magic) {
+		case CISCO_FH_MAGIC:
 			len = sizeof(ciscoflash_filehdr) - sizeof(magic);
-		else if(*magic == CISCO_FH_EXT_MAGIC || *magic == CISCO_FH_EXT_MAGIC_SWAP)
+			break;
+
+		case CISCO_FH_EXT_MAGIC:
 			len = sizeof(ciscoflash_filehdr_ext) - sizeof(magic);
-		else {
-			fprintf(stderr, "Bad magic: 0x%8.8X\n", *magic);
-			goto error;
+			break;
+
+		case 0xffffffff:
+			printf("End of filesystem\n");
+			eof = 1;
+			continue;
 		}
+
 		if(read(fd, buf+sizeof(*magic), len) == len) {
-			uint16_t crc;
-			if(*magic == CISCO_FH_MAGIC || ntohl(*magic)== CISCO_FH_MAGIC) {
+			if(*magic == CISCO_FH_MAGIC) {
 				ciscoflash_filehdr *hdr = (ciscoflash_filehdr *)buf;
-				//if(ntohl(*magic)== CISCO_FH_MAGIC)
-					swap_header(hdr);
-				dump_header(hdr);
-				p = malloc(hdr->length+3);
+				swap_header(hdr);
+				
+				p = malloc(hdr->length+1);
+				memset(p, 0, hdr->length+1);
 				if(!p)
 					goto error;
-				memset(p, 0, hdr->length+3);
 				if(read(fd, p, hdr->length) != hdr->length)
 					goto error;
-				crc = (uint16_t)calc_crc16(p, hdr->length, hdr->crc);
-				if(crc != hdr->crc)
-					printf("Bad CRC 0x%4.4X != 0x%4.4X\n", crc, hdr->crc);
+				dump_header(hdr, calc_chk16(p, hdr->length));
 				free(p);
-				printf("\n\n");
 				lseek(fd, ((hdr->length +3) & ~3) - hdr->length, SEEK_CUR);
 						
 			}
-			else if(*magic == CISCO_FH_EXT_MAGIC || *magic == CISCO_FH_EXT_MAGIC_SWAP)
+			else if(*magic == CISCO_FH_EXT_MAGIC)
 				dump_header_ext((ciscoflash_filehdr_ext *)buf);
 		}
 	}
